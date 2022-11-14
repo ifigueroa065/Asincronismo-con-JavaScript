20b73464a1773742b79e9567c55ca88ef047d3bc
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-11-14 10:38:00 -0600

    exercise

---
+++ src/async/exercise.js
@@ -0,0 +1,9 @@
+export async function runCode() {
+    const url = 'https://domain-api-com';
+    try {
+      await fetch(url)
+    } catch (error) {
+      throw new Error('API Not Found');
+    }
+  }
+  
